62665de5eb9046567a83baf747e11b365faf3a38
Whardah <whardah25@gmail.com>
2022-12-08 22:47:28 +0700

    verification login feature with valid data

--- features/pageobjects/secure.page.ts
+++ features/pageobjects/secure.page.ts
@@ -1,17 +1,17 @@
-import { ChainablePromiseElement } from 'webdriverio';
+import { ChainablePromiseElement } from "webdriverio";
 
-import Page from './page';
+import Page from "./page";
 
 /**
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    public get flashAlert () {
-        return $('#flash');
-    }
+  /**
+   * define selectors using getter methods
+   */
+  public get welcomeText() {
+    return $("#leftPanel > p");
+  }
 }
 
 export default new SecurePage();
